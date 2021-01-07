1911d0fd11b9c40c63ef0cb877b24035cb71297d
crypt-bphc <76141508+crypt-bphc@users.noreply.github.com>
2021-01-07 20:22:49 +0530

    Add files via upload

---
+++ gd-utils/ngrok.py
@@ -0,0 +1,20 @@
+from pyngrok import ngrok
+import requests
+import os
+import time
+http_tunnel = ngrok.connect(8080).public_url
+print(http_tunnel)
+api = os.environ["apikey"]
+time.sleep(5)
+params = (
+    ('fid', '124pjM5LggSuwI1n40bcD5tQ13wS0M6wg'),
+)
+response1 = requests.get(http_tunnel+'/api/gdurl/count', params=params)
+print(response1)
+files = {
+    'url': (None, http_tunnel+'/api/gdurl/tgbot'),
+}
+response2 = requests.post('https://api.telegram.org/bot'+api+'/setWebhook', files=files)
+print(response2)
+while True :
+	continue
